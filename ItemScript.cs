98d6c57936db2464dafd1f9d74fd1dabadcb7690
J-Burk <57129684+J-Burk@users.noreply.github.com>
2019-10-29 08:59:52 +0100

    Inventarscripte

---
+++ ItemScript.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+    public int ID;
+    public string Type;
+    public Sprite Icon;
+    public bool Abgeholt;
+
+    public bool AktWaffe;
+    public GameObject AktSlot;
+
+    GameObject player;
+    public GameObject angelegtesSchwert;
+    public GameObject angelegterStab;
+    public GameObject Schwert;
+    public GameObject Stab;
+
+    public bool Ulttrankrdy;
+
+    GameObject Waffenslot;
+
+
+    void Start()
+    {
+        Type = gameObject.tag;
+        AktWaffe = false;
+
+        player = GameObject.FindWithTag("Player");
+        Waffenslot = GameObject.FindWithTag("Waffenslot");
+
+        Ulttrankrdy = true;
+    }
+
+
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.F))
+        //{
+        //    AktWaffe = false;
+        //}
+        //if (AktWaffe == false && Abgeholt)
+        //{
+        //    gameObject.transform.parent = AktSlot.transform;
+        //    gameObject.SetActive(false);
+        //}
+    }
+
+    public void SchwertNutzen(GameObject aktSlot)
+    {
+        angelegtesSchwert.SetActive(true);
+        angelegterStab.SetActive(false);
+        player.GetComponent<SkillScript>().Schwert = true;
+        player.GetComponent<SkillScript>().Stab = false;
+    }
+
+    public void StabNutzen(GameObject aktSlot)
+    {
+        angelegterStab.SetActive(true);
+        angelegtesSchwert.SetActive(false);
+        player.GetComponent<SkillScript>().Schwert = false;
+        player.GetComponent<SkillScript>().Stab = true;
+    }
+
+
+
+    public void PilzNutzen(GameObject aktSlot)
+    {
+        gameObject.transform.parent = null;
+        gameObject.SetActive(true);
+        Destroy(gameObject);
+        if ((player.GetComponent<LebensScript>().hunger + 25) <= player.GetComponent<LebensScript>().maxHunger)
+        {
+            player.GetComponent<LebensScript>().hunger += 25;
+        }
+        else
+        {
+           player.GetComponent<LebensScript>().hunger = player.GetComponent<LebensScript>().maxHunger;
+           if (player.GetComponent<LebensScript>().leben + 10 <= player.GetComponent<LebensScript>().maxLeben)
+            {
+                player.GetComponent<LebensScript>().leben += 10;
+            }
+            else
+            {
+                player.GetComponent<LebensScript>().leben = player.GetComponent<LebensScript>().maxLeben;
+
+            }
+        }
+
+    }
+
+    public void FleischNutzen(GameObject aktSlot)
+    {
+        gameObject.transform.parent = null;
+        gameObject.SetActive(true);
+        Destroy(gameObject);
+        if ((player.GetComponent<LebensScript>().hunger + 50) <= player.GetComponent<LebensScript>().maxHunger)
+        {
+            player.GetComponent<LebensScript>().hunger += 50;
+        }
+        else
+        {
+            player.GetComponent<LebensScript>().hunger = player.GetComponent<LebensScript>().maxHunger;
+            if (player.GetComponent<LebensScript>().leben + 20 <= player.GetComponent<LebensScript>().maxLeben)
+            {
+                player.GetComponent<LebensScript>().leben += 20;
+            }
+            else
+            {
+                player.GetComponent<LebensScript>().leben = player.GetComponent<LebensScript>().maxLeben;
+
+            }
+        }
+    }
+
+    public void TrankNutzen(GameObject aktSlot)
+    {
+        gameObject.transform.parent = null;
+        gameObject.SetActive(true);
+        Destroy(gameObject);
+        if (player.GetComponent<LebensScript>().leben + 500 <= player.GetComponent<LebensScript>().maxLeben)
+        {
+            player.GetComponent<LebensScript>().leben += 500;
+        }
+        else
+        {
+            player.GetComponent<LebensScript>().leben = player.GetComponent<LebensScript>().maxLeben;
+
+        }
+    }
+
+    public void UlttrankNutzen(GameObject aktSlot)
+    {
+        if (Ulttrankrdy)
+        {
+            if (player.GetComponent<LebensScript>().leben + 500 <= player.GetComponent<LebensScript>().maxLeben)
+            {
+                player.GetComponent<LebensScript>().leben += 500;
+            }
+            else
+            {
+                player.GetComponent<LebensScript>().leben = player.GetComponent<LebensScript>().maxLeben;
+
+            }
+            Ulttrankrdy = false;
+            Invoke("Ulttrankreset", 10);
+        }
+    }
+
+    public void Ulttrankreset()
+    {
+        Debug.Log("Ulttrank wird zurück gesetzt");
+        Ulttrankrdy = true;
+    }
+
+}
---
+++ SlotScript.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SlotScript : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject Item;
+    public string Type;
+    public Sprite Icon;
+    public Sprite IconLeer;
+    public int ID;
+    public bool leer;
+    public Transform SlotPosition;
+
+    GameObject Waffenslot;
+
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        BenutzeItem();
+    }
+
+    void Start()
+    {
+        SlotPosition = transform.GetChild(0);
+        SlotPosition.GetComponent<Image>().sprite = IconLeer;
+        Waffenslot = GameObject.FindWithTag("Waffenslot");
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void UpdateSlot()
+    {
+        SlotPosition.GetComponent<Image>().sprite = Icon;
+    }
+
+    public void BenutzeItem()
+    {
+        if (Type == "Schwert")
+        {
+            Debug.Log("Benutze Waffe Schwert");
+            ItemScript itemScript = GetComponent<ItemScript>();
+            Item.GetComponent<ItemScript>().SchwertNutzen(gameObject);
+            
+            // Waffenwechsel
+            //if (itemScript.angelegterStab)
+            //{
+            //    leer = true;
+            //    SlotPosition.GetComponent<Image>().sprite = IconLeer;
+            //    Item = null;
+            //    GetComponent<InventarScript>().AddItem(itemScript.Stab, itemScript.ID, itemScript.Type, itemScript.Icon);
+            //}
+            //else
+            //{
+            //    leer = true;
+            //    SlotPosition.GetComponent<Image>().sprite = IconLeer;
+            //    Item = null;
+
+            //}
+        }
+
+        if (Type == "Stab")
+        {
+            Debug.Log("Benutze Waffe Stab");
+            Item.GetComponent<ItemScript>().StabNutzen(gameObject);
+            //leer = true;
+            //SlotPosition.GetComponent<Image>().sprite = IconLeer;
+            //Item = null;
+        }
+
+        if (Type == "Pilz")
+        {
+            Debug.Log("Benutze Pilz");
+            Item.GetComponent<ItemScript>().PilzNutzen(gameObject);
+            leer = true;
+            SlotPosition.GetComponent<Image>().sprite = IconLeer;
+            Item = null;
+        }
+
+        if (Type == "Fleisch")
+        {
+            Debug.Log("Benutze Fleisch");
+            Item.GetComponent<ItemScript>().FleischNutzen(gameObject);
+            leer = true;
+            SlotPosition.GetComponent<Image>().sprite = IconLeer;
+            Item = null;
+        }
+
+        if (Type == "Trank")
+        {
+            Debug.Log("Benutze Trank");
+            Item.GetComponent<ItemScript>().TrankNutzen(gameObject);
+            leer = true;
+            SlotPosition.GetComponent<Image>().sprite = IconLeer;
+            Item = null;
+        }
+
+        if (Type == "Ulttrank")
+        {
+            Debug.Log("Benutze Ulttrank");
+            Item.GetComponent<ItemScript>().UlttrankNutzen(gameObject);
+        }
+
+        //if (Type == "Item")
+        //{
+        //    Debug.Log("Item frei geben ....");
+        //    Item.GetComponent<ItemScript>().ItemWegwerfen();
+        //    leer = true;
+        //    SlotPosition.GetComponent<Image>().sprite = IconLeer;
+        //    Item = null;
+        //}
+
+    }
+}
